67e1d97e6d2f1d32359f9393a7a18578ed86a12d
walshak123 <patrickwalshak1@gmail.com>
2025-09-04 22:22:21 +0100

    bank task

--- bank.py
+++ bank.py
@@ -13,3 +13,31 @@ Example Usage:
     acc.withdraw(200)
     print(acc.get_balance())  # 1300
 """
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposite_money(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return f"Deposited: {amount}. New balance: {self.balance}"
+    def withdraw_money(self, amount):
+        if amount > self.balance:
+            return "Insufficient balance"
+        if amount <= 0:
+            return "Withdrawal amount must be positive"
+        self.balance -= amount
+        #return "Withdraw: {amount}. New balance is: {self.balance}"
+    
+    def get_balance(self):
+        return f"Balance for {self.owner}: {self.balance}"
+
+        
+account1 = BankAccount("Alice", 1000)
+print(account1.get_balance())
+
+print(account1.deposite_money(500))
+print(account1.withdraw_money(200))
+
+print(account1.get_balance())
