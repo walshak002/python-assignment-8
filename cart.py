96d00fa5faf925e4edef16cd5d855da543adbbde
walshak123 <patrickwalshak1@gmail.com>
2025-09-04 22:23:26 +0100

    calculator task

--- cart.py
+++ cart.py
@@ -16,3 +16,39 @@ Example Usage:
     cart.remove_item("Shirt", 1)
     print(cart.calculate_total())  # 5000
 """
+class ShoppingCart:
+    def __init__(self):
+        self.cart = {}
+
+    def add(self, item, quantity=1):
+        if item in self.cart:
+            self.cart[item] += quantity
+        else:
+            self.cart[item] = quantity
+    def remove(self, item):
+        if item in self.cart:
+            del self.cart[item]
+        else:
+            f"{item} not fount in cart"
+    def clear(self, item):
+        self.cart.clear()
+
+    def show_cart(self):
+        if not self.cart:
+            return "Cart is empty."
+        return self.cart
+
+
+    
+cart = ShoppingCart()
+
+cart.add("Apple", 1)
+cart.add("Banana", 2)
+cart.add("Orange", 3)
+cart.add("Mango", 4)
+
+cart.remove("Mango")
+
+print(cart.show_cart())
+
+price = {"shirt": 5000, "shoes": 120000}
